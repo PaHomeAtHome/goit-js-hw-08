2151dd5cc7f6847fa6aa5f50d85b5951a425d58b
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-05 13:21:53 +0300

    task 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe)
+
+    player.setCurrentTime(localStorage.getItem(`videoplayer-current-time`))
+    
+        player.on(`timeupdate`, throttle(function ({seconds}) {
+            localStorage.setItem("videoplayer-current-time", seconds)
+        }, 1000))
